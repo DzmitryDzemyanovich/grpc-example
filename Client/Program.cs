ea5122a6e09da7d5d2ad0175811fd170c277e9db
Dzmitry <kelus@tut.by>
2020-01-19 21:22:42 +0100

    add everything

---
+++ Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Orders;
+using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var ordersClient = new OrderPlacement.OrderPlacementClient(channel);
+            Console.WriteLine("Welcome to the gRPC client");
+            var reply = await ordersClient.CreateOrderAsync(new CreateOrderRequest
+            {
+                ProductId = "ABC1234",
+                Quantity = 1,
+                Address = "Mock Address"
+            });
+            Console.WriteLine($"Created order: {reply.OrderId}");
+
+
+            using (var statusReplies = ordersClient.GetOrderStatus(new GetOrderStatusRequest { OrderId = reply.OrderId }))
+            {
+                while (await statusReplies.ResponseStream.MoveNext())
+                {
+                    var statusReply = statusReplies.ResponseStream.Current.Status;
+                    Console.WriteLine($"Order status: {statusReply}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
